0146fbf789797155f22874d2a19f98d4ee846737
Eric Gan <eric.g@talkme.up.co>
2022-05-13 18:11:59 -0400

    videos

--- color_tracking/integrated.py
+++ color_tracking/integrated.py
@@ -31,10 +31,11 @@ while True:
     # webcam in image frames
     frame = webcam.read()
     imageFrame = frame[1] if args.get("video", False) else frame
-
     if imageFrame is None:
         break
-
+    height = imageFrame.shape[0]
+    width = imageFrame.shape[1]
+    black_frame = np.zeros((height, width, 3), dtype="uint8")
     # Convert the imageFrame in
     # BGR(RGB color space) to
     # HSV(hue-saturation-value)
@@ -43,21 +44,33 @@ while True:
 
     # Set range for red color and
     # define mask
-    red_lower = np.array([136, 87, 111], np.uint8)
-    red_upper = np.array([180, 255, 255], np.uint8)
+    red_lower = np.array([0, 182, 20], np.uint8)
+    red_upper = np.array([5, 255, 255], np.uint8)
     red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
 
     # Set range for green color and
     # define mask
-    green_lower = np.array([25, 52, 72], np.uint8)
-    green_upper = np.array([102, 255, 255], np.uint8)
+    green_lower = np.array([44, 87, 20], np.uint8)
+    green_upper = np.array([65, 255, 255], np.uint8)
     green_mask = cv2.inRange(hsvFrame, green_lower, green_upper)
 
     # Set range for blue color and
     # define mask
-    blue_lower = np.array([94, 80, 2], np.uint8)
-    blue_upper = np.array([120, 255, 255], np.uint8)
+    blue_lower = np.array([90, 199, 20], np.uint8)
+    blue_upper = np.array([106, 255, 255], np.uint8)
     blue_mask = cv2.inRange(hsvFrame, blue_lower, blue_upper)
+
+    # Set range for orange color and
+    # define mask
+    orange_lower = np.array([10, 227, 20], np.uint8)
+    orange_upper = np.array([16, 255, 255], np.uint8)
+    orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
+
+    # Set range for purple color and
+    # define mask
+    purple_lower = np.array([122, 129, 20], np.uint8)
+    purple_upper = np.array([132, 175, 255], np.uint8)
+    purple_mask = cv2.inRange(hsvFrame, purple_lower, purple_upper)
 
     # Morphological Transform, Dilation
     # for each color and bitwise_and operator
@@ -80,6 +93,16 @@ while True:
     res_blue = cv2.bitwise_and(imageFrame, imageFrame,
                                mask=blue_mask)
 
+    # For orange color
+    orange_mask = cv2.dilate(orange_mask, kernal)
+    res_orange = cv2.bitwise_and(imageFrame, imageFrame,
+                               mask=orange_mask)
+
+    # For purple color
+    purple_mask = cv2.dilate(purple_mask, kernal)
+    res_purple = cv2.bitwise_and(imageFrame, imageFrame,
+                               mask=purple_mask)
+
     # Creating contour to track red color
     contours, hierarchy = cv2.findContours(red_mask,
                                            cv2.RETR_TREE,
@@ -93,6 +116,8 @@ while True:
             center = (x + w//2, y + h//2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//2, (0, 0, 255), 2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//10, (0, 0, 255), -1)
+            black_frame = cv2.circle(black_frame, center, max(w, h) // 10, (0, 0, 255), -1)
+
 
     # Creating contour to track green color
     contours, hierarchy = cv2.findContours(green_mask,
@@ -106,6 +131,7 @@ while True:
             center = (x + w // 2, y + h // 2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//2, (0, 255, 0), 2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//10, (0, 255, 0), -1)
+            black_frame = cv2.circle(black_frame, center, max(w, h) // 10, (0, 255, 0), -1)
 
     # Creating contour to track blue color
     contours, hierarchy = cv2.findContours(blue_mask,
@@ -118,10 +144,39 @@ while True:
             center = (x + w // 2, y + h // 2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//2, (255, 0, 0), 2)
             imageFrame = cv2.circle(imageFrame, center, max(w, h)//10, (255, 0, 0), -1)
+            black_frame = cv2.circle(black_frame, center, max(w, h) // 10, (255, 0, 0), -1)
+
+    # Creating contour to track orange color
+    contours, hierarchy = cv2.findContours(orange_mask,
+                                            cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > 300:
+            x, y, w, h = cv2.boundingRect(contour)
+            center = (x + w // 2, y + h // 2)
+            imageFrame = cv2.circle(imageFrame, center, max(w, h) // 2, (0, 165, 255), 2)
+            imageFrame = cv2.circle(imageFrame, center, max(w, h) // 10, (0, 165, 255), -1)
+            black_frame = cv2.circle(black_frame, center, max(w, h) // 10, (0, 165, 255), -1)
+
+    # Creating contour to track purple color
+    contours, hierarchy = cv2.findContours(purple_mask,
+                                            cv2.RETR_TREE,
+                                            cv2.CHAIN_APPROX_SIMPLE)
+    for pic, contour in enumerate(contours):
+        area = cv2.contourArea(contour)
+        if area > 300:
+            x, y, w, h = cv2.boundingRect(contour)
+            center = (x + w // 2, y + h // 2)
+            imageFrame = cv2.circle(imageFrame, center, max(w, h) // 2, (180, 105, 255), 2)
+            imageFrame = cv2.circle(imageFrame, center, max(w, h) // 10, (180, 105, 255), -1)
+            black_frame = cv2.circle(black_frame, center, max(w, h) // 10, (180, 105, 255), -1)
+
 
     # Program Termination
     # show the frame to our screen
     cv2.imshow("Multiple Color Detection in Real-Time", imageFrame)
+    cv2.imshow('Dots on Black Frame', black_frame)
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'q' key is pressed, stop the loop
